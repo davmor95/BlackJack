f4ca292b11e8f6dcb6ae51df5adc6a2d04d7cf37
David Morales <davmorales@ucdavis.edu>
2020-06-12 00:07:27 -0400

    Added a function

--- src/BlackJackRunner.java
+++ src/BlackJackRunner.java
@@ -1,4 +1,13 @@
 
 public class BlackJackRunner {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StartGame();
+
+	}
+	public static void StartGame() {
+		System.out.println("Welcome to Black Jack!");
+	}
+
 }
